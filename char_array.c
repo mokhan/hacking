e55e0beae554ff4e37a46df5921a6120cddb8d9b
mo khan <mo@mokhan.ca>
2014-07-20 19:01:25 -0600

    add char_array example.

---
+++ char_array.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(int argc, const char *argv[])
+{
+  char message[20];
+  message[0] = 'H';
+  message[1] = 'e';
+  message[2] = 'l';
+  message[3] = 'l';
+  message[4] = 'o';
+  printf("%s", message);
+  return 0;
+}
