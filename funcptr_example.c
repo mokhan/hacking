1d0412b1355276f62ac87aeea1d936e2aa07065b
mo khan <mo@mokhan.ca>
2014-07-24 21:40:07 -0600

    add example on function pointers.

---
+++ funcptr_example.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int func_one()
+{
+  printf("This is function one\n");
+  return 1;
+}
+
+int func_two()
+{
+  printf("this is function two\n");
+  return 2;
+}
+
+int main(int argc, const char *argv[])
+{
+  int value;
+  int (*function_ptr) ();
+  function_ptr = func_one;
+  printf("function_ptr is %p\n", function_ptr);
+  value = function_ptr();
+  printf("value returned was %d\n", value);
+
+  function_ptr = func_two;
+  printf("function_ptr is %p\n", function_ptr);
+  value = function_ptr();
+  printf("value returned was %d\n", value);
+  return 0;
+}
